0d613a893f91d3a47589aed3c5336616be21c8b8
Vaporous <pablommruiz04@gmail.com>
2023-06-12 23:24:13 +0200

    Documentation and format changes

---
+++ self_check_page_47.py
@@ -0,0 +1,5 @@
+"""
+Write two Python functions to find the minimum number in a list. The first function should
+compare each number to every other number on the list. O(n^2). The second function should be
+linear O(n)
+"""
